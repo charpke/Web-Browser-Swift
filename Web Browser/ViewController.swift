35641b3de1ab2bd4f2f458858cfa57bf325674ba
Charles Harpke <charpke@gmail.com>
2015-10-26 11:32:38 -0500

    Initial Commit

--- Web Browser/ViewController.swift
+++ Web Browser/ViewController.swift
@@ -8,12 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var address: String = String()
+    
+    
 
+    @IBOutlet weak var webAddress: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBAction func goPressed(sender: AnyObject) {
+        webAddress.resignFirstResponder()
+        loadWebPage()
+    }
+    
+    
+    @IBAction func leftPressed(sender: AnyObject) {
+        webView.goBack()
+    }
+    
+    @IBAction func rightPressed(sender: AnyObject) {
+        webView.goForward()
+    }
+    
+    @IBAction func zoomInPressed(sender: AnyObject) {
+        webView.scrollView.zoomScale += 0.1
+    }
+    
+    @IBAction func zoomOutPressed(sender: AnyObject) {
+        webView.scrollView.zoomScale -= 0.1
+    }
+    func loadWebPage() {
+    
+        if webAddress.text != "" {
+        
+            address = address.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+            address = self.webAddress.text!
+            
+            if address.hasPrefix("www.") {
+                address = "http://" + address
+                
+            }else if !address.hasPrefix("http://") {
+            
+                address = "http://" + address
+            }
+            
+            let url = NSURL(string: address)
+            let request = NSURLRequest(URL: url!)
+            webView.loadRequest(request)
+            
+            
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webAddress.delegate = self
+        webView.scalesPageToFit = true
+        
+        
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        webAddress.resignFirstResponder()
+        loadWebPage()
+        return true
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
